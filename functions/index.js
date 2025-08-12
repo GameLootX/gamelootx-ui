1625fba35e9cc5bd410ea95b32c4557a7b95b1d2
GameLootX <runjan.singh4@gmail.com>
2025-08-13 02:48:13 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 const supabase = createClient(
   "https://fzduhyreyrwczebvtsmg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6ZHVoeXJleXJ3Y3plYnZ0c21nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4OTgyMDMsImV4cCI6MjA3MDQ3NDIwM30.6tGss5avISLM76KRsxyZ-ub36Zsn5S4xzDu-JTr78"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6ZHVoeXJleXJ3Y3plYnZ0c21nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4OTgyMDMsImV4cCI6MjA3MDQ3NDIwM30.6tGss5av_ISLM76KRsxyZ-ub3_6Zsn5S4xzDu-JTr78"
 );
 
 export async function onRequest() {
